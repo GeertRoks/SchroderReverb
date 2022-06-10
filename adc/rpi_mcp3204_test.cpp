882dbe14b13abccbb61cf8039d1e9e088ddeb7d0
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2022-06-10 12:11:55 +0100

    Stable sample frequency of just below 20KHz

--- adc/rpi_mcp3204_test.cpp
+++ adc/rpi_mcp3204_test.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <thread>
 #include <queue>
+#include <array>
 
 #include "task_audio_io.h"
 
@@ -13,18 +14,19 @@ int main() {
     std::queue<int> adc_output;
     std::queue<int> dac_input;
 
-    auto start_time = std::chrono::steady_clock::now();
     std::chrono::nanoseconds duration;
+
+    //auto start_time = std::chrono::steady_clock::now();
 
     std::thread adc_task(TaskAudioIO(), &adc_output, &dac_input, &duration);
     adc_task.join();
 
-    auto stop_time = std::chrono::steady_clock::now();
+    //auto stop_time = std::chrono::steady_clock::now();
 
     //auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop_time - start_time);
 
     std::cout << "execution time: " << duration.count() << " nanoseconds" << std::endl;
-    std::cout << "sample frequency: " << 10/(duration.count() * 10E-9) << " Hz" << std::endl;
+    std::cout << "sample frequency: " << 100000/(duration.count() * 1E-9) << " Hz" << std::endl;
 
     while (!adc_output.empty()) {
         o << adc_output.front() << std::endl;
--- adc/task_audio_io.cpp
+++ adc/task_audio_io.cpp
@@ -9,13 +9,14 @@ TaskAudioIO::~TaskAudioIO() {
 }
 
 void TaskAudioIO::operator()(std::queue<int>* fifo_adc_out, std::queue<int>* fifo_dac_in, std::chrono::nanoseconds* duration) {
-	int counter = 10;
+	int counter = 100000;
 	auto start = std::chrono::steady_clock::now();
 	auto stop = std::chrono::steady_clock::now();
 	std::chrono::nanoseconds elapsed = stop - start;
 	std::chrono::nanoseconds timeToWait = period - elapsed;
 	std::chrono::time_point<std::chrono::steady_clock> start_time;
 	std::chrono::time_point<std::chrono::steady_clock> stop_time;
+	std::chrono::time_point<std::chrono::steady_clock> loop_time;
     	start_time = std::chrono::steady_clock::now();
 	while (counter > 0) {
 		start = std::chrono::steady_clock::now();
@@ -29,10 +30,24 @@ void TaskAudioIO::operator()(std::queue<int>* fifo_adc_out, std::queue<int>* fif
 		timeToWait = period - elapsed;
 		// if time is left in the period then wait until the period is over
 		if (timeToWait > std::chrono::microseconds::zero()) {
-			//std::cout << "time cycle: " << counter-10 << ", time to wait: " << timeToWait.count() << " nanoseconds" <<std::endl;
-			std::this_thread::sleep_for(timeToWait);
+			//std::cout << "time cycle: " << 10-counter << ", start: " << std::chrono::duration_cast<std::chrono::nanoseconds>(start - start_time).count() << " ns, stop: " << std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start_time).count() << " ns, time to wait: " << timeToWait.count() << " ns, loop_time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(loop_time - start_time).count() << std::endl;
+
+            // Give the thread to the scheduler and get it back after the wait time is over (not accurate)
+			//std::this_thread::sleep_for(timeToWait);
+
+            // give the thread to the next in line while waiting (can be short or long)
+			//do {
+            //    std::this_thread::yield();
+            //} while (std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - stop) < timeToWait);
+
+            //busy waiting (not giving the thread back to the scheduler)
+            while(elapsed < period) {
+                elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start);
+                //std::cout << "loop: " << 10-counter << ", elapsed: " << elapsed.count() << std::endl;
+            }
 		}
 		counter--;
+		//loop_time = std::chrono::steady_clock::now();
 	}
     	stop_time = std::chrono::steady_clock::now();
    	*duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop_time - start_time);
