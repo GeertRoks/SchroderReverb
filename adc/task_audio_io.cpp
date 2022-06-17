5a6ced734e55abc20943dde7be25d5a662689ef0
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2022-06-17 14:10:09 +0100

    Add buffersize + rebuild adc task for float

--- adc/task_audio_io.cpp
+++ adc/task_audio_io.cpp
@@ -1,22 +1,26 @@
 #include "task_audio_io.h"
 #include <iostream>
 
-TaskAudioIO::TaskAudioIO() : period(std::chrono::microseconds(40)),  buffersize(64) {
+template<std::size_t buffersize>
+TaskAudioIO<buffersize>::TaskAudioIO() : period(std::chrono::microseconds(40)) {
     adc.begin(SPI_0, BCM2835_SPI_CS0);
     pwm_init();
 }
 
-TaskAudioIO::TaskAudioIO(int init_buffersize, int init_period_us) : period(std::chrono::microseconds(init_period_us)), buffersize(init_buffersize) {
+template<std::size_t buffersize>
+TaskAudioIO<buffersize>::TaskAudioIO(int init_period_us) : period(std::chrono::microseconds(init_period_us)) {
     adc.begin(SPI_0, BCM2835_SPI_CS0);
     pwm_init();
 }
 
-TaskAudioIO::~TaskAudioIO() {
+template<std::size_t buffersize>
+TaskAudioIO<buffersize>::~TaskAudioIO() {
     bcm2835_pwm_set_mode(0, 1, 0);
     bcm2835_pwm_set_mode(1, 1, 0);
 }
 
-void TaskAudioIO::run(std::queue<int>* fifo_adc_out, std::queue<int>* fifo_dac_in) {
+template<std::size_t buffersize>
+void TaskAudioIO<buffersize>::run(std::queue<float>* fifo_adc_out, std::queue<float>* fifo_dac_in) {
 
 	auto start = std::chrono::steady_clock::now();
 	auto stop = std::chrono::steady_clock::now();
@@ -28,14 +32,31 @@ void TaskAudioIO::run(std::queue<int>* fifo_adc_out, std::queue<int>* fifo_dac_i
 
         // -------------v Do audio stuff v---------------------
 
-		// retrieve sample from adc
-		fifo_adc_out->push(this->adc.readADC(CH0_1));
-        // send sample to dac
-        if (!fifo_dac_in->empty()) {
-            bcm2835_pwm_set_data(1, fifo_dac_in->front() & 0x3F);
-            bcm2835_pwm_set_data(0, fifo_dac_in->front() >> 6);
-            fifo_dac_in->pop();
+
+        if (idx_buffer_adc >= buffersize) {
+            // empty buffer and send to fifo
+            for (auto sample : buffer_adc) {
+                fifo_adc_out->push(sample);
+            }
+            idx_buffer_adc = 0;
         }
+        // retrieve sample from adc
+        tmp_adc = (this->adc.readADC(CH0_1)/2048.0f)-1.0f;
+        buffer_adc[idx_buffer_adc++] = tmp_adc;
+        
+        if (idx_buffer_dac >= buffersize) {
+            if (fifo_dac_in->size() >= buffersize) {
+                for (unsigned short i = 0; i < buffersize; i++) {
+                    buffer_dac[i] = fifo_dac_in->front();
+                    fifo_dac_in->pop();
+                }
+                idx_buffer_dac = 0;
+            }
+        }
+        // send sample to dac
+        tmp_dac = (buffer_dac[idx_buffer_dac++] + 1.0f)*2048.0f;
+        bcm2835_pwm_set_data(1, tmp_dac & 0x3F);
+        bcm2835_pwm_set_data(0, tmp_dac >> 6);
 
         // -------------^ Do audio stuff ^---------------------
 
@@ -57,7 +78,8 @@ void TaskAudioIO::run(std::queue<int>* fifo_adc_out, std::queue<int>* fifo_dac_i
 
 
 // debug variant of run()
-void TaskAudioIO::run_debug(std::queue<int>* fifo_adc_out, std::queue<int>* fifo_dac_in, std::chrono::nanoseconds* duration, int counter) {
+template<std::size_t buffersize>
+void TaskAudioIO<buffersize>::run_debug(std::queue<int>* fifo_adc_out, std::queue<int>* fifo_dac_in, std::chrono::nanoseconds* duration, int counter) {
     int count = counter;
 
 	auto start = std::chrono::steady_clock::now();
@@ -107,7 +129,8 @@ void TaskAudioIO::run_debug(std::queue<int>* fifo_adc_out, std::queue<int>* fifo
 
 }
 
-void TaskAudioIO::pwm_init() {
+template<std::size_t buffersize>
+void TaskAudioIO<buffersize>::pwm_init() {
     bcm2835_gpio_fsel(18, BCM2835_GPIO_FSEL_ALT5); //pwm0
     bcm2835_gpio_fsel(13, BCM2835_GPIO_FSEL_ALT0); //pwm1
     bcm2835_pwm_set_clock(2); // PWM clock divider: 19.2MHz/ clock_div
--- main.cpp
+++ main.cpp
@@ -5,23 +5,29 @@
 
 #include "schrodingersReverb.h"
 #include "adc/task_audio_io.h"
+#include "adc/task_audio_io.cpp"
 
 int main() {
-    TaskAudioIO audio_io;
+    const std::size_t buffersize = 64;
+    TaskAudioIO<buffersize> audio_io;
     SchrodingersReverb reverb(0);
 
     // FIFOs: naming convention: fifo_<source>_<sink>
-    std::queue<int> fifo_adc_reverb;
-    std::queue<int> fifo_reverb_dac;
+    std::queue<float> fifo_adc_reverb;
+    std::queue<float> fifo_reverb_dac;
 
-    std::thread audio_task(&TaskAudioIO::run, audio_io, &fifo_adc_reverb, &fifo_reverb_dac);
+    std::thread audio_task(&TaskAudioIO<buffersize>::run, audio_io, &fifo_adc_reverb, &fifo_reverb_dac);
+    //std::thread audio_task(&TaskAudioIO<buffersize>::run, audio_io, &fifo_adc_reverb, &fifo_adc_reverb);
 
 
     std::cout << "Schroeder Reverb is running..." << std::endl;
+    float sample = 0.0f;
     while (1) {
         if (!fifo_adc_reverb.empty()) {
-            fifo_reverb_dac.push(fifo_adc_reverb.front());
+            sample = reverb.process(fifo_adc_reverb.front());
+            //fifo_reverb_dac.push(fifo_adc_reverb.front());
             fifo_adc_reverb.pop();
+            fifo_reverb_dac.push(sample);
         }
     };
     return 0;
