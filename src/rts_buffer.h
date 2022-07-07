a9309b586eca9f05439ee5cb58bc72f84e85afad
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2022-07-07 21:55:22 +0200

    Restore: rts_buffer and its test to 1 capacity

--- src/rts_buffer.h
+++ src/rts_buffer.h
@@ -12,65 +12,43 @@ class RTS_Buffer {
     public:
         RTS_Buffer(std::size_t buffersize) {
           this->buffersize = buffersize;
-          this->capacity = 1;
-          buffers = new T*[capacity];
-          for (uint16_t i = 0; i < capacity; i++) {
-              buffers[i] = new T[buffersize];
-          }
+          buffer = new T[buffersize];
+          isEmpty = true;
         }
         RTS_Buffer(std::size_t buffersize, std::size_t capacity) {
           this->buffersize = buffersize;
           this->capacity = capacity;
-          buffers = new T*[capacity];
-          for (uint16_t i = 0; i < capacity; i++) {
-              buffers[i] = new T[buffersize];
-          }
+          buffer = new T[buffersize*capacity];
+          isEmpty = true;
         }
         ~RTS_Buffer() {
-          for (uint16_t i = 0; i < capacity; i++) {
-              delete buffers[i];
-              buffers[i] = nullptr;
-          }
-          delete buffers;
-          buffers = nullptr;
+          delete buffer;
+          buffer = nullptr;
         }
 
         void read(T* buffer_out) {
-            std::unique_lock<std::mutex> locker(mu);
-            if (isEmpty()) { 
-                throw std::runtime_error("Buffer is empty");
-                cond.wait(locker, [this](){ return !isEmpty(); });
-            }
-            std::copy(&buffers[read_head%capacity][0], &buffers[read_head%capacity][buffersize], buffer_out);
+          std::unique_lock<std::mutex> locker(mu);
+          cond.wait(locker, [this](){ return !isEmpty; });
 
-            if (read_head == capacity) {
-                read_head = 1;
-            } else {
-                read_head++;
-            }
+          std::copy(buffer, &buffer[buffersize], buffer_out);
+          isEmpty = true;
 
-            locker.unlock();
+          locker.unlock();
         }
         bool write(T* buffer_in) {
-            if (isFull()) {
-                //throw std::runtime_error("Buffer is full");
-                cond.notify_one();
-                return 0;
-            } else {
-                std::unique_lock<std::mutex> locker(mu);
+          if (isEmpty) {
+            std::unique_lock<std::mutex> locker(mu);
 
-                std::copy(buffer_in, &buffer_in[buffersize], &buffers[write_head%capacity][0]);
+            std::copy(buffer_in, &buffer_in[buffersize], buffer);
+            isEmpty = false;
 
-                if (write_head == capacity) {
-                    write_head = 1;
-                } else {
-                    write_head++;
-                }
-
-                locker.unlock();
-                cond.notify_one();
-                return 1;
-            }
+            locker.unlock();
+            cond.notify_one();
+            return 1;
+          } else {
+            cond.notify_one();
+            return 0;
+          }
         }
 
         std::mutex* getMutex() {
@@ -80,21 +58,12 @@ class RTS_Buffer {
           return &cond;
         }
 
-        bool isEmpty() {
-            return (read_head == write_head);
-        }
-        bool isFull() {
-            return (read_head == ((write_head+1) % (capacity+1)));
-        }
-
     private:
-        T** buffers;
+        T* buffer;
+        std::atomic<bool> isEmpty;
 
         std::size_t buffersize;
         std::size_t capacity = 1;
-
-        std::size_t write_head = 0;
-        std::size_t read_head = 0;
 
         std::mutex mu;
         std::condition_variable cond;
